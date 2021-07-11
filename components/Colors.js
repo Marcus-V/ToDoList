d2c8977bff4d9f765c5cbf757560814bd030266a
6114 <6114@ait.nsw.edu.au>
2021-07-11 22:00:01 +1000

    Adding component Files

---
+++ components/Colors.js
@@ -0,0 +1,15 @@
+export const Colors = {
+    //pallet A https://coolors.co/1f2041-4b3f72-417b5a-d0ceba-e9d2c0
+    spaceCadet: "hsla(238, 35%, 19%, 1)",
+    cyberGrape: "hsla(254, 29%, 35%, 1)",
+    amazon: "hsla(146, 31%, 37%, 1)",
+    bone: "hsla(55, 19%, 77%, 1)",
+    champagnePink: "hsla(26, 48%, 83%, 1)",
+
+    //pallet B https://coolors.co/17bebb-2e282a-cd5334-edb88b-fad8d6
+    maximumBlueGreen: "hsla(179, 78%, 42%, 1)",
+    raisinBlack: "hsla(340, 7%, 17%, 1)",
+    vermilion: "hsla(12, 60%, 50%, 1)",
+    macaroniAndCheese: "hsla(28, 73%, 74%, 1)",
+    palePink: "hsla(3, 78%, 91%, 1)",
+}
